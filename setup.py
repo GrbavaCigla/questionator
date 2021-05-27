38ba7ec5c13338e3b73b4210afb5a563362f7c83
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2021-05-27 15:23:26 +0200

    Starting point

---
+++ setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name="Questionator",
+    version="1.0",
+    description="Q&A API",
+    author="Aleksa Ognjanovic",
+    author_email="alexa.ognjanovic@protonmail.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        # TODO: Add this
+    ],
+    python_requires=">=3.7",
+)
