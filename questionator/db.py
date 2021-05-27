29a9ab9aae990cafa05945f62f2b0d234dcdc238
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2021-05-27 22:33:20 +0200

    Heroku fixes

--- questionator/db.py
+++ questionator/db.py
@@ -5,11 +5,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.orm import sessionmaker
 
-from .config import settings
-
 if os.environ.get("DATABASE_URL"):
     engine = create_engine(os.environ.get("DATABASE_URL"), pool_pre_ping=True)
 else:
+    from .config import settings
     engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
