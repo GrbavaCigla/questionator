6868d820890c71033840806340a552bf6be6066d
GrbavaCigla <alexa.ognjanovic@protonmail.com>
2021-05-27 22:38:46 +0200

    Heroku fixes

--- questionator/db.py
+++ questionator/db.py
@@ -6,7 +6,10 @@ from sqlalchemy.ext.declarative import as_declarative
 from sqlalchemy.orm import sessionmaker
 
 if os.environ.get("DATABASE_URL"):
-    engine = create_engine(os.environ.get("DATABASE_URL"), pool_pre_ping=True)
+    # Heroku
+    uri = os.environ.get("DATABASE_URL")
+    uri = "postgresql" + uri[9:] 
+    engine = create_engine(uri, pool_pre_ping=True)
 else:
     from .config import settings
     engine = create_engine(settings.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
